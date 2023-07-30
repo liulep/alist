880e13e22d7183e5cde3167141d1068059e07c2a
liulep <53442063+liulep@users.noreply.github.com>
2023-07-30 19:23:57 +0800

    Delete head.js

--- head/head.js
+++
@@ -1,2 +0,0 @@
-document.write("<!--Alist V3建议添加的，已经默认添加了，如果你的没有建议加上-->");
-document.write("<script src=\"https:\/\/polyfill.io\/v3\/polyfill.min.js?features=String.prototype.replaceAll\"><\/script>");
