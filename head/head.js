2250d521c72365737ff23523b725185544990310
liulep <2422669375@qq.com>
2023-07-30 19:49:49 +0800

    create body and head file

---
+++ head/head.js
@@ -0,0 +1,4 @@
+document.write("<!--Alist V3默认添加-->");
+document.write("<script src=\"https://polyfill.io/v3/polyfill.min.js?features=String.prototype.replaceAll\"></script>");
+document.write("<!--计数器-->");
+document.write("<script async src=\"//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js\"></script>");
